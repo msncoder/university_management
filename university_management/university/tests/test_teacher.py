1e22d06352d6a62f91cc85bbd9a18c6c2f7a0129
saad <msaadnaseem.dev@gmail.com>
2026-03-10 04:09:01 +0500

    teacher test cases write

--- university_management/university/tests/test_teacher.py
+++ university_management/university/tests/test_teacher.py
@@ -8,11 +8,69 @@ def api_client():
 
 @pytest.fixture
 def department():
-    return Department.objects.create(name = "Engineering")
+    return Department.objects.create(name="Engineering")
 
 @pytest.fixture
 def teacher(department):
     return Teacher.objects.create(name="asad",department = department)
 
+@pytest.mark.django_db
+def test_create_teacher(api_client,department):
+    url = "/api/teachers/"
+    payload = {
+        "name":"New Teacher",
+        "department":department.id
+    }
+
+    response = api_client.post(url,payload,format='json')
+
+    assert response.status_code == 201
+    assert Teacher.objects.count() == 1
+    assert response.data["name"] == "New Teacher"
 
 
+@pytest.mark.django_db
+def test_list_teacher(api_client,teacher):
+    url = "/api/teachers/"
+    response = api_client.get(url)
+
+    assert response.status_code == 200
+    assert len(response.data) == 1
+    # assert response.data[0]['name'] == teacher.name
+
+
+@pytest.mark.django_db
+def test_retrieve_teacher(api_client,teacher):
+    url = f'/api/teachers/{teacher.id}/'
+
+    response = api_client.get(url)
+
+    assert response.status_code == 200
+    assert response.data["name"] == teacher.name
+
+
+@pytest.mark.django_db
+def test_update_teacher(api_client, teacher, department):
+    url = f"/api/teachers/{teacher.id}/"
+
+    payload = {
+        "name": "Updated Name",
+        "department": department.id
+    }
+
+    response = api_client.put(url, payload, format="json")
+
+    teacher.refresh_from_db()
+
+    assert response.status_code == 200
+    assert teacher.name == "Updated Name"
+
+        
+@pytest.mark.django_db
+def test_delete_teacher(api_client, teacher):
+    url = f"/api/teachers/{teacher.id}/"
+
+    response = api_client.delete(url)
+
+    assert response.status_code == 204
+    assert Teacher.objects.count() == 0
